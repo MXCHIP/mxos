1af2ebf774a491185fe384a546648591176fdbe2
yangsw <yangsw@mxchip.com>
2019-03-08 10:43:59 +0800

    使用 python 脚本代替 shell 脚本来实现 EMW3370 的 prepend_header 和 pad 的功能

--- platform/MCU/RTL8721D/build_helper.mk
+++ platform/MCU/RTL8721D/build_helper.mk
@@ -7,12 +7,12 @@ postbuild: build_done
 	$(NM) $(LINK_OUTPUT_FILE) | sort > $(NM_OUTPUT_FILE)
 	$(OBJCOPY) -j .ram_image2.entry -j .ram_image2.text -j .ram_image2.data -Obinary $(STRIPPED_LINK_OUTPUT_FILE) $(OUTPUT_DIR)/binary/ram_2.bin
 	$(OBJCOPY) -j .xip_image2.text -Obinary $(STRIPPED_LINK_OUTPUT_FILE) $(OUTPUT_DIR)/binary/xip_image2.bin
-	$(KM4_ASDK_PATH)gnu_utility/prepend_header.sh $(OUTPUT_DIR)/binary/ram_2.bin __ram_image2_text_start__ $(NM_OUTPUT_FILE)
-	$(KM4_ASDK_PATH)gnu_utility/prepend_header.sh $(OUTPUT_DIR)/binary/xip_image2.bin __flash_text_start__ $(NM_OUTPUT_FILE)
-	cat $(OUTPUT_DIR)/binary/xip_image2_prepend.bin $(OUTPUT_DIR)/binary/ram_2_prepend.bin > $(BIN_OUTPUT_FILE)
-	$(KM4_ASDK_PATH)gnu_utility/pad.sh $(BIN_OUTPUT_FILE)
-	cat $(KM4_ASDK_PATH)../../realtek_amebaD_cm0_gcc_verification/asdk/image/km0_image2_all.bin $(BIN_OUTPUT_FILE) > $(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.all.bin)
+	$(PYTHON) mxos/platform/MCU/RTL8721D/scripts/prepend_header.py $(OUTPUT_DIR)/binary/ram_2.bin __ram_image2_text_start__ $(NM_OUTPUT_FILE)
+	$(PYTHON) mxos/platform/MCU/RTL8721D/scripts/prepend_header.py $(OUTPUT_DIR)/binary/xip_image2.bin __flash_text_start__ $(NM_OUTPUT_FILE)
+	$(CAT) $(OUTPUT_DIR)/binary/xip_image2_prepend.bin $(OUTPUT_DIR)/binary/ram_2_prepend.bin > $(BIN_OUTPUT_FILE)
+	$(PYTHON) mxos/platform/MCU/RTL8721D/scripts/pad.py $(BIN_OUTPUT_FILE)
+	$(CAT) $(KM4_ASDK_PATH)../../realtek_amebaD_cm0_gcc_verification/asdk/image/km0_image2_all.bin $(BIN_OUTPUT_FILE) > $(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.all.bin)
 
 download: postbuild
 	@echo Downloading ...
-	yes|./mxos/platform/MCU/RTL8721D/flashloader/flashloader.sh $(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.all.bin) >$(OUTPUT_DIR)/flashloader.log 2>&1
+	echo yes|./mxos/platform/MCU/RTL8721D/flashloader/flashloader.sh $(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.all.bin) >$(OUTPUT_DIR)/flashloader.log 2>&1
