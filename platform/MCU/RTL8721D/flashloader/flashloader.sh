b139c13bc4dba41b660836b10f39608858858e75
yangsw <yangsw@mxchip.com>
2019-04-23 09:24:19 +0800

    - 现在执行 MX3380 SWD download 命令，会下载 KM0 和 KM4 的 boot。

--- platform/MCU/RTL8721D/flashloader/flashloader.sh
+++ platform/MCU/RTL8721D/flashloader/flashloader.sh
@@ -1,6 +1,9 @@
 cur_path=$(cd `dirname $0`; pwd)
+cp $cur_path/gnu_utility/gnu_script/rtl_gdb_flash_write.txt $cur_path/gnu_utility/gnu_script/rtl_flash_write.txt
+gsed -i "s#^set \$KM0_BOOT_ALL_SIZE =.*#set \$KM0_BOOT_ALL_SIZE =$(stat -f %z $cur_path/../image/km0_boot_all.bin)#g" $cur_path/gnu_utility/gnu_script/rtl_flash_write.txt
+gsed -i "s#^set \$KM4_BOOT_ALL_SIZE =.*#set \$KM4_BOOT_ALL_SIZE =$(stat -f %z $cur_path/../image/km4_boot_all.bin)#g" $cur_path/gnu_utility/gnu_script/rtl_flash_write.txt
 cp $1 $cur_path/image/image.bin
-gsed -i "s#^set \$XIPBootSize =.*#set \$XIPBootSize =$(stat -f %z $1)#g" $cur_path/gnu_utility/gnu_script/rtl_gdb_flash_write.txt
+gsed -i "s#^set \$KM0_KM4_IMAGE2_SIZE =.*#set \$KM0_KM4_IMAGE2_SIZE =$(stat -f %z $1)#g" $cur_path/gnu_utility/gnu_script/rtl_flash_write.txt
 cd $cur_path
 ./JLinkGDBServerCLExe -device cortex-m23 -if SWD -s &
-./arm-none-eabi-gdb -x gnu_utility/gnu_script/rtl_gdb_flash_write.txt
+./arm-none-eabi-gdb -x gnu_utility/gnu_script/rtl_flash_write.txt
