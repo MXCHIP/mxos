2347059879299bf25d4f4483ef9b3dd37ff793d9
yangsw <yangsw@mxchip.com>
2019-05-21 11:47:50 +0800

    style: change some variable name in "mos.h".

--- include/mos.h
+++ include/mos.h
@@ -15,9 +15,9 @@ typedef void *mos_timer_id_t;
 typedef void (*mos_thread_func_t)(void *arg);
 typedef void (*mos_timer_handler_t)(void *arg);
 
-#define MOS_NEVER_TIMEOUT   (0xFFFFFFFF)
-#define MOS_WAIT_FOREVER    (0xFFFFFFFF)
-#define MOS_NO_WAIT         (0)
+#define MOS_NEVER_TIMEOUT (0xFFFFFFFF)
+#define MOS_WAIT_FOREVER (0xFFFFFFFF)
+#define MOS_NO_WAIT (0)
 
 typedef struct
 {
@@ -44,7 +44,7 @@ void mos_thread_yield(void);
 void mos_thread_join(mos_thread_id_t id);
 void mos_thread_awake(mos_thread_id_t id);
 mos_thread_id_t mos_thread_get_id(void);
-char *mos_thread_get_name(mos_thread_id_t id);
+const char *mos_thread_get_name(mos_thread_id_t id);
 
 // semaphore
 mos_semphr_id_t mos_semphr_new(uint32_t count);
@@ -59,18 +59,18 @@ void mos_mutex_unlock(mos_mutex_id_t id);
 void mos_mutex_delete(mos_mutex_id_t id);
 
 // queue
-mos_queue_id_t mos_queue_new(uint32_t message_size, uint32_t number_of_messages);
-merr_t mos_queue_push(mos_queue_id_t id, void *message, uint32_t timeout_ms);
-merr_t mos_queue_pop(mos_queue_id_t id, void *message, uint32_t timeout_ms);
+mos_queue_id_t mos_queue_new(uint32_t size, uint32_t number);
+merr_t mos_queue_push(mos_queue_id_t id, void *data, uint32_t timeout);
+merr_t mos_queue_pop(mos_queue_id_t id, void *data, uint32_t timeout);
 void mos_queue_delete(mos_queue_id_t id);
 uint32_t mos_queue_get_total(mos_queue_id_t id);
 uint32_t mos_queue_get_free(mos_queue_id_t id);
 
 // timer
-mos_timer_id_t mos_timer_new(uint32_t time_ms, mos_timer_handler_t function, bool autoreload, void *arg);
+mos_timer_id_t mos_timer_new(uint32_t timeout, mos_timer_handler_t function, bool repeat, void *arg);
 void mos_timer_delete(mos_timer_id_t id);
 void mos_timer_stop(mos_timer_id_t id);
-void mos_timer_change(mos_timer_id_t id, uint32_t time_ms);
+void mos_timer_change(mos_timer_id_t id, uint32_t timeout);
 void mxos_timer_reset(mos_timer_id_t id);
 bool mos_timer_is_runing(mos_timer_id_t id);
 
