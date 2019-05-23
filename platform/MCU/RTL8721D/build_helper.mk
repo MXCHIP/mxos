7949f405db4dcb444d3da47acd28282b2a1795a3
杨海波 <yanghb@mxchip.com>
2019-05-23 15:26:37 +0800

    3380的all.bin包含了所有的固件：boot、ate、application download的时候会完整的下载固件 ATE放到ota1，application放到ota2 更新km0的boot，支持通过拉低boot和elink脚进入ATE模式。

--- platform/MCU/RTL8721D/build_helper.mk
+++ platform/MCU/RTL8721D/build_helper.mk
@@ -3,6 +3,21 @@ EXTRA_POST_BUILD_TARGETS += postbuild
 ADD_MD5_SCRIPT := $(SOURCE_ROOT)/mxos/platform/MCU/RTL8721D/scripts/add_md5.py
 
 NM_OUTPUT_FILE := $(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.nm)
+GEN_COMMON_BIN_OUTPUT_FILE_SCRIPT:= $(SCRIPTS_PATH)/gen_common_bin_output_file.py
+ALL_BIN_OUTPUT_FILE :=$(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.all$(BIN_OUTPUT_SUFFIX))
+OTA_BIN_OUTPUT_FILE :=$(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.ota$(BIN_OUTPUT_SUFFIX))
+
+
+KM0_BOOT_FILE    := mxos/platform/MCU/RTL8721D/image/km0_boot_all.bin
+KM0_BOOT_OFFSET  := 0x0
+
+KM4_BOOT_FILE    := mxos/platform/MCU/RTL8721D/image/km4_boot_all.bin
+KM4_BOOT_OFFSET  := 0x4000
+
+ATE_FILE    := mxos/platform/MCU/RTL8721D/image/ate.bin
+ATE_OFFSET  := 0x6000
+
+OTA_OFFSET  := 0x188000
 
 ifeq ($(HOST_OS),Win32)
 
@@ -25,11 +40,15 @@ postbuild: build_done
 	$(PYTHON) mxos/platform/MCU/RTL8721D/scripts/prepend_header.py $(OUTPUT_DIR)/binary/xip_image2.bin __flash_text_start__ $(NM_OUTPUT_FILE)
 	$(CAT) $(OUTPUT_DIR)/binary/xip_image2_prepend.bin $(OUTPUT_DIR)/binary/ram_2_prepend.bin > $(BIN_OUTPUT_FILE)
 	$(PYTHON) mxos/platform/MCU/RTL8721D/scripts/pad.py $(BIN_OUTPUT_FILE)
-	$(CAT) $(SOURCE_ROOT)/mxos/platform/MCU/RTL8721D/image/km0_image2_all.bin $(BIN_OUTPUT_FILE) > $(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.all.bin)
+	$(CAT) $(SOURCE_ROOT)/mxos/platform/MCU/RTL8721D/image/km0_image2_all.bin $(BIN_OUTPUT_FILE) > $(OTA_BIN_OUTPUT_FILE)
 endif
+	$(PYTHON) $(ADD_MD5_SCRIPT) $(OTA_BIN_OUTPUT_FILE)
+	$(QUIET)$(RM) $(ALL_BIN_OUTPUT_FILE)
+	$(PYTHON) $(GEN_COMMON_BIN_OUTPUT_FILE_SCRIPT) -o $(ALL_BIN_OUTPUT_FILE) -f $(KM0_BOOT_OFFSET)   $(KM0_BOOT_FILE)              
+	$(PYTHON) $(GEN_COMMON_BIN_OUTPUT_FILE_SCRIPT) -o $(ALL_BIN_OUTPUT_FILE) -f $(KM4_BOOT_OFFSET)   $(KM4_BOOT_FILE)
+	$(PYTHON) $(GEN_COMMON_BIN_OUTPUT_FILE_SCRIPT) -o $(ALL_BIN_OUTPUT_FILE) -f $(ATE_OFFSET)        $(ATE_FILE) 
+	$(PYTHON) $(GEN_COMMON_BIN_OUTPUT_FILE_SCRIPT) -o $(ALL_BIN_OUTPUT_FILE) -f $(OTA_OFFSET)        $(OTA_BIN_OUTPUT_FILE) 
 
-	$(CP) $(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.all.bin) $(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.ota.bin)
-	$(PYTHON) $(ADD_MD5_SCRIPT) $(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.ota.bin)
 	$(QUIET)$(call MKDIR, $(BUILD_DIR)/eclipse_debug/)
 	$(QUIET)$(CP) $(LINK_OUTPUT_FILE) $(BUILD_DIR)/eclipse_debug/last_built.elf
 
@@ -39,5 +58,5 @@ ifeq ($(HOST_OS),Win32)
 # echo yes|./mxos/platform/MCU/RTL8721D/flashloader/flashloader.sh $(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.all.bin) $(OPENOCD_FULL_NAME) >$(OUTPUT_DIR)/flashloader.log 2>&1
 else	
 	@echo Downloading
-	echo yes|./mxos/platform/MCU/RTL8721D/flashloader/flashloader.sh $(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=.all.bin) $(OPENOCD_FULL_NAME) >$(OUTPUT_DIR)/flashloader.log 2>&1
+	echo yes|./mxos/platform/MCU/RTL8721D/flashloader/flashloader.sh $(OTA_BIN_OUTPUT_FILE) $(OPENOCD_FULL_NAME) >$(OUTPUT_DIR)/flashloader.log 2>&1
 endif
--- platform/MCU/RTL8721D/flashloader/flashloader.sh
+++ platform/MCU/RTL8721D/flashloader/flashloader.sh
@@ -2,6 +2,7 @@ cur_path=$(cd `dirname $0`; pwd)
 cp $cur_path/gnu_utility/gnu_script/rtl_gdb_flash_write.txt $cur_path/gnu_utility/gnu_script/rtl_flash_write.txt
 gsed -i "s#^set \$KM0_BOOT_ALL_SIZE =.*#set \$KM0_BOOT_ALL_SIZE =$(stat -f %z $cur_path/../image/km0_boot_all.bin)#g" $cur_path/gnu_utility/gnu_script/rtl_flash_write.txt
 gsed -i "s#^set \$KM4_BOOT_ALL_SIZE =.*#set \$KM4_BOOT_ALL_SIZE =$(stat -f %z $cur_path/../image/km4_boot_all.bin)#g" $cur_path/gnu_utility/gnu_script/rtl_flash_write.txt
+gsed -i "s#^set \$ATE_IMAGE_SIZE =.*#set \$ATE_IMAGE_SIZE =$(stat -f %z $cur_path/../image/ate.bin)#g" $cur_path/gnu_utility/gnu_script/rtl_flash_write.txt
 cp $1 $cur_path/image/image.bin
 gsed -i "s#^set \$KM0_KM4_IMAGE2_SIZE =.*#set \$KM0_KM4_IMAGE2_SIZE =$(stat -f %z $1)#g" $cur_path/gnu_utility/gnu_script/rtl_flash_write.txt
 cd $cur_path
