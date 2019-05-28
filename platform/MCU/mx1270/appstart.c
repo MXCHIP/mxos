27f9244ca86a27ccdf39aae308bbfe278f2f87e0
yangsw <yangsw@mxchip.com>
2019-05-28 15:32:44 +0800

    [startup] move aos components init to "usr_main" function. [flash layout] change application address to 0xA000. [flash alg] download total will download bootloader, application and ate image.

--- platform/MCU/mx1270/appstart.c
+++ platform/MCU/mx1270/appstart.c
@@ -2,9 +2,26 @@
 
 #include "mxos_rtos.h"
 
+typedef struct
+{
+    int argc;
+    char **argv;
+    bool cli_enable;
+} kinit_t;
+
+const kinit_t kinit =
+    {
+        .cli_enable = true,
+};
+
 int mxos_debug_enabled;
 mos_mutex_id_t stdio_tx_mutex;
 
+int32_t vfs_init(void);
+void cli_service_init(kinit_t *kinit);
+void ulog_init(const char host_name[8]);
+void vfs_device_init(void);
+void aos_loop_init(void);
 void aos_loop_run(void);
 int main(int argc, const char *argv[]);
 
@@ -20,6 +37,12 @@ static void usrapp_thread(void *arg)
 
 void usr_main(void)
 {
+    vfs_init();
+    cli_service_init(&kinit);
+    ulog_init("A");
+    vfs_device_init();
+    aos_loop_init();
+
     mos_thread_new(32, "user app thread", usrapp_thread, 2048, NULL);
 
     aos_loop_run();
--- platform/MCU/mx1270/flash_alg.mk
+++ platform/MCU/mx1270/flash_alg.mk
@@ -1,4 +1,23 @@
+BOOT_BIN_FILE := $(MXOS_OS_PATH)/platform/MCU/mx1270/image/boot.bin
+ATE_BIN_FILE := $(MXOS_OS_PATH)/platform/MCU/mx1270/image/ate.bin
+
+ifneq (total,$(findstring total,$(MAKECMDGOALS)))
 download: $(BIN_OUTPUT_FILE) display_map_summary
 	$(eval IMAGE_SIZE := $(shell $(PYTHON) $(IMAGE_SIZE_SCRIPT) $(BIN_OUTPUT_FILE)))
-	$(QUIET)$(ECHO) Downloading image, size: $(IMAGE_SIZE) bytes... 
+	$(QUIET)$(ECHO) Downloading applcation, size: $(IMAGE_SIZE) bytes... 
 	$(PYTHON) mxos/makefiles/scripts/flash_alg_progressbar/flash_alg_write.py -o $(OPENOCD_FULL_NAME) -f $(BIN_OUTPUT_FILE) -a 0x40000 -m mx1270
+else
+download: $(BIN_OUTPUT_FILE) display_map_summary
+	$(eval IMAGE_SIZE := $(shell $(PYTHON) $(IMAGE_SIZE_SCRIPT) $(BOOT_BIN_FILE)))
+	$(QUIET)$(ECHO) Downloading bootloader, size: $(IMAGE_SIZE) bytes... 
+	$(PYTHON) mxos/makefiles/scripts/flash_alg_progressbar/flash_alg_write.py -o $(OPENOCD_FULL_NAME) -f $(BOOT_BIN_FILE) -a 0x00 -m mx1270
+	$(eval IMAGE_SIZE := $(shell $(PYTHON) $(IMAGE_SIZE_SCRIPT) $(BIN_OUTPUT_FILE)))
+	$(QUIET)$(ECHO) Downloading applcation, size: $(IMAGE_SIZE) bytes... 
+	$(PYTHON) mxos/makefiles/scripts/flash_alg_progressbar/flash_alg_write.py -o $(OPENOCD_FULL_NAME) -f $(BIN_OUTPUT_FILE) -a 0xA000 -m mx1270
+	$(eval IMAGE_SIZE := $(shell $(PYTHON) $(IMAGE_SIZE_SCRIPT) $(ATE_BIN_FILE)))
+	$(QUIET)$(ECHO) Downloading ate, size: $(IMAGE_SIZE) bytes... 
+	$(PYTHON) mxos/makefiles/scripts/flash_alg_progressbar/flash_alg_write.py -o $(OPENOCD_FULL_NAME) -f $(ATE_BIN_FILE) -a 0x100000 -m mx1270
+endif
+
+total:
+	@:
