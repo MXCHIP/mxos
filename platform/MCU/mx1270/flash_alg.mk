0fc72a83e95e3003608e7af0cde9ca80afe9755c
yangsw <yangsw@mxchip.com>
2019-05-28 16:04:58 +0800

    fix: use the correct application address in flash_alg.mk

--- platform/MCU/mx1270/flash_alg.mk
+++ platform/MCU/mx1270/flash_alg.mk
@@ -5,7 +5,7 @@ ifneq (total,$(findstring total,$(MAKECMDGOALS)))
 download: $(BIN_OUTPUT_FILE) display_map_summary
 	$(eval IMAGE_SIZE := $(shell $(PYTHON) $(IMAGE_SIZE_SCRIPT) $(BIN_OUTPUT_FILE)))
 	$(QUIET)$(ECHO) Downloading applcation, size: $(IMAGE_SIZE) bytes... 
-	$(PYTHON) mxos/makefiles/scripts/flash_alg_progressbar/flash_alg_write.py -o $(OPENOCD_FULL_NAME) -f $(BIN_OUTPUT_FILE) -a 0x40000 -m mx1270
+	$(PYTHON) mxos/makefiles/scripts/flash_alg_progressbar/flash_alg_write.py -o $(OPENOCD_FULL_NAME) -f $(BIN_OUTPUT_FILE) -a 0xA000 -m mx1270
 else
 download: $(BIN_OUTPUT_FILE) display_map_summary
 	$(eval IMAGE_SIZE := $(shell $(PYTHON) $(IMAGE_SIZE_SCRIPT) $(BOOT_BIN_FILE)))
